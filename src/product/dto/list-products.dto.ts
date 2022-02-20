c65980c537748eea8458aaa60a386420dffd0200
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2022-02-20 17:16:24 +0300

    fix query string validation

--- src/product/dto/list-products.dto.ts
+++ src/product/dto/list-products.dto.ts
@@ -1,18 +1,21 @@
-import { IsNumber } from 'class-validator';
+import { IsInt } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ListProductsDto {
   @ApiProperty({
     example: 10,
     description: 'The total number of products to be returned',
   })
-  @IsNumber()
+  @IsInt()
+  @Type(() => Number)
   readonly limit: number;
 
   @ApiProperty({
     example: 10,
     description: 'The index to start reading from',
   })
-  @IsNumber()
+  @IsInt()
+  @Type(() => Number)
   readonly offset: number;
 }
